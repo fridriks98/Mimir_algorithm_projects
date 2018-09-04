ca7b20030463c3be15f3a86f8c6b1b965d5537c0
Fridrik Snaer <frikkifc@gmail.com>
2018-09-04 10:54:05 +0000

    Changes added

---
+++ sequence.py
@@ -0,0 +1,11 @@
+n= int(input("Enter the length of the sequence: "))
+
+what_num = "tala"
+sum = 0
+
+for seq in range(0,n):
+    for num in range(1,n+1):
+        what_num + str(num)
+        sum =+ num
+        print(sum)
+	
